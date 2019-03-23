36fb27d648db0976ba2b2ea59803d297863a2952
iqbalrevvin <iqbal.revvin@gmail.com>
2019-03-24 02:46:02 +0700

    Fixed Config Base URL
    Perbaikan konfigurasi URL dengan mengembalikan nilai normal lokasi folder aplikasi karena terdapat permasalahan saat kelola data pengguna sistem

--- application/config/config.php
+++ application/config/config.php
@@ -15,10 +15,10 @@ date_default_timezone_set('Asia/Jakarta');
 | path to your installation.
 |
 */
-//$config['base_url']	= 'http://localhost/tabunganKU/';
-$root = "http://".$_SERVER['HTTP_HOST'];
-$root = str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = "$root";
+$config['base_url']	= 'http://localhost/tabunganKU/';
+#$root = "http://".$_SERVER['HTTP_HOST'];
+#$root = str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+#$config['base_url'] = "$root";
 /*
 |--------------------------------------------------------------------------
 | Index File
